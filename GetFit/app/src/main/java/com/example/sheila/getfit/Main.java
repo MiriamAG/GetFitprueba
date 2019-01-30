67133345874eef1059e4f4d6dc51527a67f52163
miriam <gitclase@gmail.com>
2019-01-30 22:08:45 +0100

    modificaciones

--- GetFit/app/src/main/java/com/example/sheila/getfit/Main.java
+++
@@ -1,8 +0,0 @@
-package com.example.sheila.getfit;
-
-public class Main {
-
-
-
-
-}
--- GetFit/app/src/main/java/com/example/sheila/getfit/MainActivity.java
+++ GetFit/app/src/main/java/com/example/sheila/getfit/MainActivity.java
@@ -1,50 +1,25 @@
 package com.example.sheila.getfit;
 
-import android.app.Activity;
-
-import android.content.Intent;
-
-
 import android.content.IntentSender;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
+
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.Scopes;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.Scope;
 import com.google.android.gms.fitness.Fitness;
-import com.google.android.gms.fitness.FitnessOptions;
 import com.google.android.gms.fitness.data.DataPoint;
-import com.google.android.gms.fitness.data.DataSource;
-import com.google.android.gms.fitness.data.DataType;
-import com.google.android.gms.fitness.request.DataReadRequest;
 import com.google.android.gms.fitness.request.OnDataPointListener;
-import com.google.android.gms.fitness.result.DataReadResponse;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 
-
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Locale;
-import java.util.concurrent.TimeUnit;
-
-import static com.google.android.gms.common.Scopes.FITNESS_ACTIVITY_READ;
 
 public class MainActivity extends AppCompatActivity implements OnDataPointListener,
         GoogleApiClient.ConnectionCallbacks,
